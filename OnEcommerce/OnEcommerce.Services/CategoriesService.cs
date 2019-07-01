4ee7eb8497a1a08f8dcc959e0a8da542ff6044cc
nghiemhuuhoang97@outlook.com <nghiemhuuhoang97@gmail.com>
2019-07-01 15:02:59 +0700

    CRUD operator category session 2

---
+++ OnEcommerce/OnEcommerce.Services/CategoriesService.cs
@@ -0,0 +1,60 @@
+﻿using OnEcommerce.Database;
+using OnEcommrce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnEcommerce.Services
+{
+   public class CategoriesService
+    {
+        public List<Category> GetCategories()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public Category GetCategories(int ID)
+        {
+            using (var context = new CBContext())
+            {   
+
+                return context.Categories.Find(ID);
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using (var context= new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                var category = context.Categories.Find(ID);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+
+       
+    }
+}
---
+++ OnEcommerce/OnEcommerce/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using OnEcommerce.Services;
+using OnEcommrce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnEcommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoriesService = new CategoriesService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoriesService.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var categories = categoriesService.GetCategories(ID);
+            return View(categories);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoriesService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var categories = categoriesService.GetCategories(ID);
+            return View(categories);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoriesService.DeleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoriesService.SaveCategory(category);
+            return RedirectToAction("Index"); ;
+        }
+    }
+}
